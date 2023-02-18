acb35029dd101dd1866e27d4aeb14541b8e97a48
Ariel Rodrigo Colque Garcia <colqueariel023@gmail.com>
2023-02-18 16:18:36 -0400

    feat: add relation Pedido with Cliente, User and Producto

--- app/Models/Pedido.php
+++ app/Models/Pedido.php
@@ -8,4 +8,20 @@ use Illuminate\Database\Eloquent\Model;
 class Pedido extends Model
 {
     use HasFactory;
+    protected $table = 'pedidos';
+
+    public function clientes()
+    {
+        return $this->belongsTo(Cliente::class);
+    }
+
+    public function users()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function productos()
+    {
+        return $this->belongsToMany(Producto::class)->wherePivot(['cantidad']);
+    }
 }
