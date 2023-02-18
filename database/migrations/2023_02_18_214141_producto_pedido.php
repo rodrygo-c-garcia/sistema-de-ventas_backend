56b6801a7c90ad1c814a55714296489b2320b07f
Ariel Rodrigo Colque Garcia <colqueariel023@gmail.com>
2023-02-18 18:01:03 -0400

    feat: add producto_pedido

---
+++ database/migrations/2023_02_18_214141_producto_pedido.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('producto_pedido', function (Blueprint $table) {
+            $table->id();
+
+            $table->bigInteger('pedido_id')->unsigned();
+            $table->foreign('pedido_id')->references('id')->on('pedidos');
+
+            $table->bigInteger('producto_id')->unsigned();
+            $table->foreign('producto_id')->references('id')->on('productos');
+
+            $table->integer('cantidad')->default(0);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};
