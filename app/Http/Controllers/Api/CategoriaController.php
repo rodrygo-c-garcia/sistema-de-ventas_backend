fd7b52b4afa8f850961e7edb6babf40a7a2edffd
Ariel Rodrigo Colque Garcia <colqueariel023@gmail.com>
2023-02-19 09:49:40 -0400

    feat: add function index and store Categoria

--- app/Http/Controllers/Api/CategoriaController.php
+++ app/Http/Controllers/Api/CategoriaController.php
@@ -3,9 +3,30 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Categoria;
 use Illuminate\Http\Request;
 
 class CategoriaController extends Controller
 {
-    //
+    public function index()
+    {
+        $lista_categoria = Categoria::all();
+        return response()->json($lista_categoria, 200);
+    }
+
+    public function store(Request $request)
+    {
+        // validar
+        $request->validate([
+            'nombre' => 'required|unique:categorias|max:50|min:3',
+        ]);
+
+        // guardar
+        $categoria = new Categoria();
+        $categoria->nombre = $request->nombre;
+        $categoria->detalle = $request->detalle;
+        $categoria->save();
+
+        return response()->json(['mensaje' => 'Categoria Registrada', 'data' => $categoria], 201);
+    }
 }
