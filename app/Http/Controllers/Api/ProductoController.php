eff7a7f08bef2c2240ea7dd567dc7fe92850e9b1
Ariel Rodrigo Colque Garcia <colqueariel023@gmail.com>
2023-02-23 09:23:43 -0400

    feat: add function update product

--- app/Http/Controllers/Api/ProductoController.php
+++ app/Http/Controllers/Api/ProductoController.php
@@ -3,14 +3,16 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Categoria;
 use App\Models\Producto;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Redis;
 
 class ProductoController extends Controller
 {
     public function index()
     {
-        $lista_productos = Producto::all();
+        $lista_productos = Producto::whit('categoria')->all();
         return response()->json($lista_productos);
     }
 
@@ -18,21 +20,56 @@ class ProductoController extends Controller
     {
         $request->validate([
             'nombre' => 'required',
-            'precio' => 'number',
-            'stock' => 'number',
-            'imagen' => 'required|image|max:2048'
+            'precio' => 'number|required',
+            'stock' => 'number|required',
+            'precio_compra' => 'number|required',
+            'precio_venta' => 'number|required',
+            //'imagen' => 'required|image|max:2048'
         ]);
 
         $producto = new Producto();
         $producto->nombre = $request->nombre;
-        $producto->descripcion = $request->descripcion;
+        $producto->cod_barras = $request->cod_barras;
+        $producto->precio_compra = $request->precio_compra;
+        $producto->precio_venta = $request->precio_venta;
+        $producto->precio_venta = $request->precio_venta;
+        $utilidad = $request->precio_venta - $request->precio_compra;
+        $producto->utilidad = $utilidad;
         $producto->stock = $request->stock;
         $producto->imagen = $request->imagen;
         $producto->estado = $request->estado;
         $producto->categoria_id = $request->categoria_id;
-        $producto->proveedor_id = $request->proveedor_id;
         $producto->save();
 
         return response()->json(['mensaje' => 'Producto Registrado', 'data' => $producto], 201);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $producto = Producto::where('id', $id)->first();
+        // Si existe el producto procedemos a guardar
+        if ($producto) {
+            $request->validate([
+                'nombre' => 'required|required',
+                'precio' => 'number|required',
+                'stock' => 'number|required',
+                'precio_compra' => 'number|required',
+                'precio_venta' => 'number|required',
+            ]);
+
+            $producto->nombre = $request->nombre;
+            $producto->cod_barras = $request->cod_barras;
+            $producto->precio_compra = $request->precio_compra;
+            $producto->precio_venta = $request->precio_venta;
+            $utilidad = $request->precio_venta - $request->precio_compra;
+            $producto->utilidad = $utilidad;
+            $producto->stock = $request->stock;
+            $producto->estado = $request->estado;
+            $producto->categoria_id = $request->categoria_id;
+            $producto->save();
+
+            return response()->json(['mensaje' => 'Producto Modificado', 'data' => $producto], 201);
+        }
+        return response()->json(['mensaje' => 'El producto no existe'], 400);
     }
 }
