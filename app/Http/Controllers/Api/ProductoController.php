2fdc941da22d8f8f6d9c12d6a638fb73a41bac94
Ariel Rodrigo Colque Garcia <colqueariel023@gmail.com>
2023-03-01 06:42:39 -0400

    refactor: requered, number

--- app/Http/Controllers/Api/ProductoController.php
+++ app/Http/Controllers/Api/ProductoController.php
@@ -3,27 +3,24 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
-use App\Models\Categoria;
 use App\Models\Producto;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Redis;
 
 class ProductoController extends Controller
 {
     public function index()
     {
-        $lista_productos = Producto::whit('categoria')->all();
-        return response()->json($lista_productos);
+        $lista_productos = Producto::with('categoria')->get();
+        return response()->json($lista_productos, 200);
     }
 
     public function store(Request $request)
     {
         $request->validate([
             'nombre' => 'required',
-            'precio' => 'number|required',
-            'stock' => 'number|required',
-            'precio_compra' => 'number|required',
-            'precio_venta' => 'number|required',
+            'stock' => 'required',
+            'precio_compra' => 'required',
+            'precio_venta' => 'required',
             //'imagen' => 'required|image|max:2048'
         ]);
 
@@ -37,7 +34,6 @@ class ProductoController extends Controller
         $producto->utilidad = $utilidad;
         $producto->stock = $request->stock;
         $producto->imagen = $request->imagen;
-        $producto->estado = $request->estado;
         $producto->categoria_id = $request->categoria_id;
         $producto->save();
 
@@ -51,10 +47,9 @@ class ProductoController extends Controller
         if ($producto) {
             $request->validate([
                 'nombre' => 'required|required',
-                'precio' => 'number|required',
-                'stock' => 'number|required',
-                'precio_compra' => 'number|required',
-                'precio_venta' => 'number|required',
+                'stock' => 'required',
+                'precio_compra' => 'required',
+                'precio_venta' => 'required',
             ]);
 
             $producto->nombre = $request->nombre;
@@ -64,7 +59,6 @@ class ProductoController extends Controller
             $utilidad = $request->precio_venta - $request->precio_compra;
             $producto->utilidad = $utilidad;
             $producto->stock = $request->stock;
-            $producto->estado = $request->estado;
             $producto->categoria_id = $request->categoria_id;
             $producto->save();
 
