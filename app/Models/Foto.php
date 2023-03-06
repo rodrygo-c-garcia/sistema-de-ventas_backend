643a9b7b2ce033785a83937be24ea61656d62df1
Ariel Rodrigo Colque Garcia <colqueariel023@gmail.com>
2023-03-06 09:02:25 -0400

    feat: add model Foto

---
+++ app/Models/Foto.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Foto extends Model
+{
+    use HasFactory;
+    protected $table = 'fotos';
+    // asignación masiva para los atributos id y url
+    protected $fillable = ['id', 'url'];
+    public $incrementing = false;
+
+    public function productos()
+    {
+        return $this->hasMany(Producto::class);
+    }
+}
