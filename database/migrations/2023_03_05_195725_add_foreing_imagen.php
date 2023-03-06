1dcf1637a26419de5117d7feeec5dfdcf11e4417
Ariel Rodrigo Colque Garcia <colqueariel023@gmail.com>
2023-03-06 09:01:33 -0400

    feat: foreign imagen_id in productos

---
+++ database/migrations/2023_03_05_195725_add_foreing_imagen.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('productos', function (Blueprint $table) {
+            $table->string('imagen_id');
+            $table->foreign('imagen_id')->references('id')->on('fotos');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('productos', function (Blueprint $table) {
+            $table->string("imagen_id");
+            $table->foreign("imagen_id")->references("id")->on("fotos");
+        });
+    }
+};
