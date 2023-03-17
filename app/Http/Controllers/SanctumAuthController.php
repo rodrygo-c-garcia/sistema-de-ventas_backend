8f36734e334dc01cf0d6d396fa73548a54d4eac6
Ariel Rodrigo Colque Garcia <colqueariel023@gmail.com>
2023-03-17 07:04:26 -0400

    feat: add funcition login

--- app/Http/Controllers/SanctumAuthController.php
+++ app/Http/Controllers/SanctumAuthController.php
@@ -2,9 +2,34 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class SanctumAuthController extends Controller
 {
-    //
+    public function login(Request $request)
+    {
+        // validar datos
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required|string',
+        ]);
+
+        // verificamos y obtenemos al usuario por su correco
+        $user = User::where('email', $request->email)->first();
+        // verificamos si existe el id y la contraseña del usuario en la BD
+        if (isset($user->id) && Hash::check($request->password, $user->password)) {
+            // creamos el token en formato de texto plano
+            $token = $user->createToken("auth_token")->plainTextToken;
+            return response()->json([
+                "mensaje" => "Usuario Logueado",
+                "access_token" => $token,
+                "error" => false,
+                'user' => $user
+            ]);
+        } else {
+            return response()->json(["mensaje" => "Credenciales invalidas", "error" => true], 200);
+        }
+    }
 }
