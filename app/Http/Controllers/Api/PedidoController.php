ca7f44f010a8bc92c4616460946941e12acc84f7
Rodrigo <colqueariel023@gmail.com>
2023-07-04 14:53:13 -0400

    feat: add function index in pedido

--- app/Http/Controllers/Api/PedidoController.php
+++ app/Http/Controllers/Api/PedidoController.php
@@ -3,8 +3,15 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Pedido;
 use Illuminate\Http\Request;
 
 class PedidoController extends Controller
 {
+    public function index()
+    {
+        // retornamos los pedidos paginados con su cliente y el usuario
+        $pedidos = Pedido::with('cliente', 'user')->paginate(10);
+        return response()->json($pedidos, 200);
+    }
 }
