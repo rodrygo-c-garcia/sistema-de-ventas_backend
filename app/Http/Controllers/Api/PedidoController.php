6458cfef6f3005e4afd87cf4a990cb4cbcb6e5a8
Rodrigo <colqueariel023@gmail.com>
2023-07-06 19:56:03 -0400

    feat: add save product

--- app/Http/Controllers/Api/PedidoController.php
+++ app/Http/Controllers/Api/PedidoController.php
@@ -43,6 +43,7 @@ class PedidoController extends Controller
                 'utilidad' => $request->utilidad,
                 'estado' => 0
             ]);
+            $pedido->save();
 
             // Validar la cantidad de productos valido para su registro
             if (count($request->productos) > 0) {
