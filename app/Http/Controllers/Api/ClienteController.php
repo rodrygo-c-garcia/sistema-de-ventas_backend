fe8f353fa1367d9bc83740c32ea35a61ab9383bb
Rodrigo <colqueariel023@gmail.com>
2023-07-07 09:02:35 -0400

    feat: add validatWhitBag cliente

--- app/Http/Controllers/Api/ClienteController.php
+++ app/Http/Controllers/Api/ClienteController.php
@@ -7,5 +7,15 @@ use Illuminate\Http\Request;
 
 class ClienteController extends Controller
 {
-    //
+    // funcion para guardar un registro de cliente
+    public function store(Request $request)
+    {
+        $request->validateWithBag('cliente', [
+            'nombre_completo' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:clientes',
+            'telefono' => 'required|integer|min:60000000|max:79999999',
+            'nit' => 'required|integer|min:1000|max:9999',
+            'direccion' => 'required|string|max:255',
+        ]);
+    }
 }
